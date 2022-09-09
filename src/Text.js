01e5e4b839507eb6d8c22804b79793fc901a4592
Matt Fryer <fryertmatthew@gmail.com>
2022-09-09 14:11:19 -0400

    Changed Initial Text

--- src/Text.js
+++ src/Text.js
@@ -6,7 +6,7 @@ import React from "react"
 export default function Text() {
     return (
         <div class="boxStyle">
-            <h1>My first code using locally installed React!</h1>
+            <h1>I'm back baby! Ready to fully dive into React</h1>
         </div>
     )
 }
